30c2cedd4b55b6fb219c3e06ef1a202b4fb7062d
squares2 <strategyquant8@gmail.com>
2026-05-14 23:35:33 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('fetch', (event) => {
-  // Add this line to ignore Netlify functions
-  if (event.request.url.includes('/.netlify/functions/')) {
-    return; // Let the browser handle the request normally
-  }
-  
-  // ... rest of your service worker code
-});
